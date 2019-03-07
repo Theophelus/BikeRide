53eb2ff83d8d449bf08a14bb0d11c980cdafc5c7
Theophelus <aneletom10@gmail.com>
2019-03-07 16:42:53 +0200

    first commit

---
+++ src/bicycles/Bicycle.java
@@ -0,0 +1,38 @@
+package bicycles;
+
+public class Bicycle {
+
+    //define a variable called speed
+//    private int speed = 0;
+//
+//    //Define a method called acceleration
+//    public acceleration(){
+//        speed = speed + 5;
+//    }
+//
+//    //Define a method called acceleration
+//    public brake(){
+//
+//    }
+//
+//    //Define a method called acceleration
+//    public currentSpeed(){
+//
+//    }
+//    //Define a method called acceleration
+//    public stop(){
+//
+//    }
+//
+//
+//
+//
+//
+//
+
+
+//    public static void main(String [] arg){
+//        System.out.println("Hello world");
+//    }
+
+}
