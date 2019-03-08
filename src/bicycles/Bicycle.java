e60fa0ceaf64926fa31fca271e882fe4940d9258
Theophelus <aneletom10@gmail.com>
2019-03-08 12:20:29 +0200

    adding a BikeRide class with a constructor to access and call bicycle class an Object

--- src/bicycles/Bicycle.java
+++ src/bicycles/Bicycle.java
@@ -2,37 +2,20 @@ package bicycles;
 
 public class Bicycle {
 
-    //define a variable called speed
-//    private int speed = 0;
-//
-//    //Define a method called acceleration
-//    public acceleration(){
-//        speed = speed + 5;
-//    }
-//
-//    //Define a method called acceleration
-//    public brake(){
-//
-//    }
-//
-//    //Define a method called acceleration
-//    public currentSpeed(){
-//
-//    }
-//    //Define a method called acceleration
-//    public stop(){
-//
-//    }
-//
-//
-//
-//
-//
-//
+//  define a variable called speed
+    private int speed = 0;
 
+    //Define a method called acceleration
+    public void acceleration(){
+        speed += 5;
+    }
 
-//    public static void main(String [] arg){
-//        System.out.println("Hello world");
-//    }
+    //Define a method called acceleration
+    public void brake(){speed -= 3;}
+
+    //Define a method called acceleration
+    public int currentSpeed(){ return speed; }
+    //Define a method called acceleration
+    public void stop(){ speed = speed + 0; }
 
 }
---
+++ src/bicycles/BicycleRide.java
@@ -0,0 +1,18 @@
+package bicycles;
+
+public class BicycleRide
+{
+    //Define a constructor that takes in Bicyle class as an Object
+    public BicycleRide(Bicycle bicycle){
+
+        BicycleRide bicycleRide = new BicycleRide(bicycle);
+        bicycleRide.ride();
+    }
+
+    private void ride() {
+    }
+
+}
+
+
+
