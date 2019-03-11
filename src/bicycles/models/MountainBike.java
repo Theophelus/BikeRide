df0cecd1c0f53345a8ddaa722cf50116cd8f4668
Theophelus <aneletom10@gmail.com>
2019-03-11 17:25:48 +0200

    making mountainBike class use BicycleBase class

--- src/bicycles/models/MountainBike.java
+++ src/bicycles/models/MountainBike.java
@@ -2,31 +2,17 @@ package bicycles.models;
 
 import bicycles.Bicycle;
 
-public class MountainBike implements Bicycle {
+public class MountainBike extends BicycleBase {
 
     int speed = 0;
 
     @Override
     public void accelerate() {
-
-        speed += 5;
-
+        this.changeSpeed(speed += 5);
     }
 
     @Override
     public void brake() {
-        speed -= 3;
-
-    }
-
-    @Override
-    public int currentSpeed() {
-        return speed;
-    }
-
-    @Override
-    public void stop() {
-
-        speed = 0;
+        this.changeSpeed(speed -= 3);
     }
 }
