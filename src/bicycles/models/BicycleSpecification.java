bf73c3794747316b7dc5c6bd0db1eaf16d38d155
Theophelus <aneletom10@gmail.com>
2019-03-14 10:10:45 +0200

    adding a constructor and two getter methods on BicycleSpecification class

--- src/bicycles/models/BicycleSpecification.java
+++ src/bicycles/models/BicycleSpecification.java
@@ -2,5 +2,23 @@ package bicycles.models;
 
 public class BicycleSpecification {
 
-    
+
+    //Define two properties for acceleration and Breaks
+    private int accelerationSpeed;
+    private int brake;
+
+    //define a constructor to be able to call instance of BicycleSpecs
+   public BicycleSpecification(int accelerationSpeed, int brake){
+       this.accelerationSpeed = accelerationSpeed;
+       this.brake = brake;
+    }
+
+    //Define two getter method for acceleration and bake
+    public int getAccelerationSpeed() {
+        return accelerationSpeed;
+    }
+
+    public int getBrake() {
+        return brake;
+    }
 }
