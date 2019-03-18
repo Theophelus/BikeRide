baba155774dbdb4abbc29fdab48f18d3bd7aa2bf
Theophelus <aneletom10@gmail.com>
2019-03-18 16:42:36 +0200

    defining a private variable and a constructor of FunRide class so that I can be able to access that class

--- src/bicycles/models/FunRide.java
+++ src/bicycles/models/FunRide.java
@@ -1,5 +1,23 @@
 package bicycles.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FunRide {
+
+    //Define a ArrayList to hold a list of Bicycle
+    List <Object> List = new ArrayList<Object>();
+
+    private int maximumNumnerOfBikes;
+
+    //Define a constructor that take maximumNumber as a parameter
+    public FunRide( int maximumNumberOfBikes){
+        this.maximumNumnerOfBikes = maximumNumberOfBikes;
+
+    }
+
+    //Define accept method here.
+
+
 
 }
